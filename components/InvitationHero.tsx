6a72d69dc7e15199d0173f6e7b598fd4d5db1654
Shubham Patil <shubhampatilsd@gmail.com>
2023-09-26 10:36:21 -0700

    fading updates

--- components/InvitationHero.tsx
+++ components/InvitationHero.tsx
@@ -1,7 +1,7 @@
 export const InvitationHero = () => {
   return (
-    <div className="bg-[url('/alleyway-background.png')] bg-left-top w-screen h-screen justify-center flex flex-col items-center">
-      <div className="px-6 pt-12 w-[80%] max-w-5xl">
+    <div className="bg-[url('/alleyway-background.png')] bg-left-top w-screen h-screen justify-center flex flex-col relative items-center">
+      <div className="px-6 w-[80%] max-w-5xl">
         <button className="card relative aspect-[1421/904] h-auto w-full  mx-auto">
           <img
             src="/quainthouse.png"
@@ -14,8 +14,8 @@ export const InvitationHero = () => {
         </button>
       </div>
       {/* <h1 className="text-7xl  text-accent-default">The Haunted House</h1> */}
-      <div className="pointer-events-none fixed bg-gradient-to-b from-transparent from-50% z-[99] to-black w-full h-full"></div>
-      <div className="pointer-events-none static bg-gradient-to-b from-transparent from-10% z-[99] to-black w-full h-full"></div>
+      <div className="pointer-events-none fixed bg-gradient-to-b from-transparent from-80% md:from-50% z-[99] to-black w-full h-full"></div>
+      <div className="pointer-events-none  bg-gradient-to-b from-transparent h-[30%] absolute bottom-0 from-0% z-[99] to-black w-full"></div>
 
       {/* <div class=""></div> */}
     </div>
