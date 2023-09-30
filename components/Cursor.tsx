55706d5d9b6bdd37506acc01741058bb13b411a0
Ivoine <ivoinestrachan242@gmail.com>
2023-09-29 22:08:51 -0400

    Reverting changes

--- components/Cursor.tsx
+++ components/Cursor.tsx
@@ -1,5 +1,4 @@
 import { motion } from "framer-motion";
-import Image from "next/image";
 
 // Spring animated cursor
 export default function Cursor({ color, x, y }) {
@@ -29,6 +28,11 @@ export default function Cursor({ color, x, y }) {
 // SVG cursor shape
 function CursorSvg({ color }) {
   return (
-   <Image src="/ouijasmallish.png" alt="cursor" width={32} height={44}/>
+    <svg width="32" height="44" viewBox="0 0 24 36" fill="none">
+      <path
+        fill={color}
+        d="M5.65376 12.3673H5.46026L5.31717 12.4976L0.500002 16.8829L0.500002 1.19841L11.7841 12.3673H5.65376Z"
+      />
+    </svg>
   );
 }
