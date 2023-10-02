f1b09574c3ef29a07b7f884cf6d3912465c74a58
Ivoine <ivoinestrachan242@gmail.com>
2023-10-01 21:36:04 -0400

    arrow disappears ons croll

--- components/InvitationHero.tsx
+++ components/InvitationHero.tsx
@@ -1,6 +1,25 @@
 import { BsChevronDoubleDown } from "react-icons/bs";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+
 
 export const InvitationHero = () => {
+
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollThreshold = 100;
+      setIsVisible(window.scrollY < scrollThreshold);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="bg-[url('/alleyway-background-compressed.webp')] bg-left-top w-screen h-screen justify-center flex flex-col relative items-center">
       <div className="px-4 md:px-6 w-full md:w-[80%] max-w-5xl">
@@ -17,8 +36,15 @@ export const InvitationHero = () => {
       </div>
 
       {/* <h1 className="text-7xl  text-accent-default">The Haunted House</h1> */}
+     
       <div className="pointer-events-none fixed bg-gradient-to-b from-transparent from-80% md:from-80% z-[99] to-black w-full h-full"></div>
-      <div className="pointer-events-none flex justify-center bg-gradient-to-b from-transparent h-[30%] absolute bottom-0 from-0% z-[99] to-black w-full">
+      {isVisible && (
+      <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+      className="pointer-events-none flex justify-center bg-gradient-to-b from-transparent h-[30%] absolute bottom-0 from-0% z-[99] to-black w-full">
         <BsChevronDoubleDown
           style={
             {
@@ -28,7 +54,9 @@ export const InvitationHero = () => {
           className="text-beige  animate-bounce absolute bottom-10"
           size={40}
         />
-      </div>
+       
+      </motion.div>
+       )}
 
       {/* <div class=""></div> */}
     </div>
