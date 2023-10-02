6d7a1bd7797c7a3115ea114fc774ac288c827ede
Ivoine <ivoinestrachan242@gmail.com>
2023-10-01 21:43:06 -0400

    wrong order

--- components/Signup.tsx
+++ components/Signup.tsx
@@ -151,7 +151,7 @@ export const Signup = ({ bgPlay, bgStop }) => {
               </p>
             </div>
             <div className="space-y-5" id="signup">
-              <div className="flex-col flex space-y-1">
+            <div className="flex-col flex space-y-1">
                 <label className="text-2xl font-black text-beige">Name</label>
                 <input
                   type="text"
@@ -160,6 +160,16 @@ export const Signup = ({ bgPlay, bgStop }) => {
                   placeholder="Ex: Orpheus Smith"
                 />
               </div>
+              <div className="flex-col flex space-y-1">
+                <label className="text-2xl font-black text-beige">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  className="text-xl px-4 py-2 bg-beige text-blackish placeholder-gray-500 tracking-wide border-2 rounded-lg border-accent-darker "
+                  placeholder="Ex: orpheus@hackclub.com"
+                />
+              </div>
+         
 
               <div className="flex-col flex space-y-1">
                 <label className="text-2xl font-black text-beige">Age</label>
@@ -203,16 +213,6 @@ export const Signup = ({ bgPlay, bgStop }) => {
                   <p className="text-beige text-lg h-min">{showError}</p>
                 </div>
               </div>
-              <div className="flex-col flex space-y-1">
-                <label className="text-2xl font-black text-beige">Email</label>
-                <input
-                  type="email"
-                  name="email"
-                  className="text-xl px-4 py-2 bg-beige text-blackish placeholder-gray-500 tracking-wide border-2 rounded-lg border-accent-darker "
-                  placeholder="Ex: orpheus@hackclub.com"
-                />
-              </div>
-
               <input
                 type="submit"
                 value={"Sign Up"}
