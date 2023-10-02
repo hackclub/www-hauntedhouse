6834746d4c382cfa583013c5f96376bdde7f418d
Shubham Patil <shubhampatilsd@gmail.com>
2023-10-02 09:50:25 -0700

    changed postcard

--- components/InvitationHero.tsx
+++ components/InvitationHero.tsx
@@ -27,7 +27,7 @@ export const InvitationHero = () => {
             className={`card-side front shadow-2xl shadow-red-500  border-[0.8rem]   transition border-white`}
           />
           <img
-            src="/smallerpostcard.webp"
+            src="/changedpostcard.webp"
             className={`card-side back shadow-2xl shadow-red-500  border-[0.8rem]   transition border-white`}
           />
         </button>
