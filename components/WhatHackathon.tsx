2d19113fe693c6b851003d1292c2fc9436706b87
Shubham Patil <shubhampatilsd@gmail.com>
2023-10-03 01:21:35 -0700

    sticky

--- components/WhatHackathon.tsx
+++ components/WhatHackathon.tsx
@@ -26,24 +26,111 @@ export const WhatHackathon = () => {
       <div className="flex flex-col max-w-[90rem] mx-auto lg:flex-row  divide-x-0 divide-accent-default divide-dashed">
         <div className="p-8 md:p-12  lg:w-[40%]">
           {/* <img src="/cardimages/tape.png" /> */}
-          <div className="relative pt-10 py-2 px-2 flex flex-col h-full justify-center items-center">
-            <img
+          <div className="relative flex flex-wrap pt-10 py-2 px-2  h-full justify-center items-center">
+            {/* <img
               src="/stacked1.png"
               className="absolute opacity-0 sm:opacity-100  scale-105 aspect-[83/100] mx-auto my-auto"
             />
             <img
               src="/stacked2.png"
               className="absolute opacity-0 sm:opacity-100 scale-105 aspect-[83/100] mx-auto my-auto"
-            />
-            <div className=" bg-accent-default pb-4 relative aspect-[83/100] w-full border border-accent-darker shadow-2xl max-w-lg mx-auto my-auto p-[30px]">
+            /> */}
+            <div className=" bg-accent-default pb-4 relative aspect-square w-full border border-accent-darker shadow-2xl max-w-lg mx-auto my-auto p-[30px]">
               <img
                 src="/cardimages/tape.webp"
                 className="w-[26%] max-w-[7rem] -top-[15%] left-[45%] absolute z-50"
               />
               <div className="overflow-hidden relative">
-                <div
-                  className={` bg-[url('/outernet-127.webp')] bg-center bg-cover aspect-[90/100] lg:aspect-[94/100] w-full`}
+                <h1 className="pb-4 marker text-2xl lg:text-2xl xl:text-3xl mt-5 relative z-50 font-black text-beige">
+                  Flamethrower at{" "}
+                  <a
+                    href="https://www.youtube.com/watch?v=O1s5HqSqKi0"
+                    className="underline"
+                  >
+                    Outernet
+                  </a>
+                </h1>
+              </div>
+              <div className="w-full h-full absolute top-0 left-0 overflow-hidden">
+                <img
+                  src="/cardimages/witch.webp"
+                  className="absolute z-0 -left-5 -bottom-5 w-[25%]"
                 />
+              </div>
+              <div className="w-full h-full absolute top-0 left-0 overflow-hidden">
+                <img
+                  src="/cardimages/web.webp"
+                  className="absolute z-0 -right-10  -bottom-10 w-[40%]"
+                />
+              </div>
+            </div>
+
+            <div className=" bg-accent-default pb-4 relative aspect-square w-full border border-accent-darker shadow-2xl max-w-lg mx-auto my-auto p-[30px]">
+              <img
+                src="/cardimages/tape.webp"
+                className="w-[26%] max-w-[7rem] -top-[15%] left-[45%] absolute z-50"
+              />
+              <div className="overflow-hidden relative">
+                <h1 className="pb-4 marker text-2xl lg:text-2xl xl:text-3xl mt-5 relative z-50 font-black text-beige">
+                  Flamethrower at{" "}
+                  <a
+                    href="https://www.youtube.com/watch?v=O1s5HqSqKi0"
+                    className="underline"
+                  >
+                    Outernet
+                  </a>
+                </h1>
+              </div>
+              <div className="w-full h-full absolute top-0 left-0 overflow-hidden">
+                <img
+                  src="/cardimages/witch.webp"
+                  className="absolute z-0 -left-5 -bottom-5 w-[25%]"
+                />
+              </div>
+              <div className="w-full h-full absolute top-0 left-0 overflow-hidden">
+                <img
+                  src="/cardimages/web.webp"
+                  className="absolute z-0 -right-10  -bottom-10 w-[40%]"
+                />
+              </div>
+            </div>
+
+            <div className=" bg-accent-default pb-4 relative aspect-square w-full border border-accent-darker shadow-2xl max-w-lg mx-auto my-auto p-[30px]">
+              <img
+                src="/cardimages/tape.webp"
+                className="w-[26%] max-w-[7rem] -top-[15%] left-[45%] absolute z-50"
+              />
+              <div className="overflow-hidden relative">
+                <h1 className="pb-4 marker text-2xl lg:text-2xl xl:text-3xl mt-5 relative z-50 font-black text-beige">
+                  Flamethrower at{" "}
+                  <a
+                    href="https://www.youtube.com/watch?v=O1s5HqSqKi0"
+                    className="underline"
+                  >
+                    Outernet
+                  </a>
+                </h1>
+              </div>
+              <div className="w-full h-full absolute top-0 left-0 overflow-hidden">
+                <img
+                  src="/cardimages/witch.webp"
+                  className="absolute z-0 -left-5 -bottom-5 w-[25%]"
+                />
+              </div>
+              <div className="w-full h-full absolute top-0 left-0 overflow-hidden">
+                <img
+                  src="/cardimages/web.webp"
+                  className="absolute z-0 -right-10  -bottom-10 w-[40%]"
+                />
+              </div>
+            </div>
+
+            <div className=" bg-accent-default pb-4 relative aspect-square w-full border border-accent-darker shadow-2xl max-w-lg mx-auto my-auto p-[30px]">
+              <img
+                src="/cardimages/tape.webp"
+                className="w-[26%] max-w-[7rem] -top-[15%] left-[45%] absolute z-50"
+              />
+              <div className="overflow-hidden relative">
                 <h1 className="pb-4 marker text-2xl lg:text-2xl xl:text-3xl mt-5 relative z-50 font-black text-beige">
                   Flamethrower at{" "}
                   <a
@@ -75,7 +162,7 @@ export const WhatHackathon = () => {
             Why Haunted House?
           </h1>
           <div className="space-y-8">
-            <Definition />
+            {/* <Definition /> */}
             <RoomProvider
               id={roomId}
               initialPresence={{
