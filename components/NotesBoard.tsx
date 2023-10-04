cb2de5995e65041cd1409079e81fa8091a756c89
faisalsayed10 <faisal.sayed502@gmail.com>
2023-10-04 12:54:49 +0530

    drag drop notes board

---
+++ components/NotesBoard.tsx
@@ -0,0 +1,100 @@
+import { useEffect, useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
+const items = [
+  { id: 1, content: "1. Come up with an idea" },
+  { id: 2, content: "2. Form a team" },
+  { id: 3, content: "3. Build and learn" },
+  { id: 4, content: "4. Show off what you made" },
+];
+
+const NotesBoard = () => {
+  const [state, setState] = useState<{ id: number; content: string }[]>(items);
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result as { id: number; content: string }[];
+  };
+
+  return (
+    <DragDropContext
+      enableDefaultSensors
+      onDragEnd={(result) => {
+        if (!result.destination) return;
+
+        const items = reorder(state, result.source.index, result.destination.index);
+        setState(items);
+      }}
+    >
+      <Row items={state} />
+    </DragDropContext>
+  );
+};
+
+const Row = ({ items }) => {
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    const animation = requestAnimationFrame(() => setEnabled(true));
+    return () => {
+      cancelAnimationFrame(animation);
+      setEnabled(false);
+    };
+  }, []);
+
+  if (!enabled) return null;
+
+  return (
+    <Droppable droppableId="droppable" direction="horizontal">
+      {(provided, snapshot) => (
+        <div
+          ref={provided.innerRef}
+          className="w-full px-8 grid grid-cols-1 md:grid-cols-2 lg:flex flex-wrap lg:flex-nowrap gap-16  lg:gap-8 max-w-6xl mx-auto bg-[url('/corkboard.webp')] bg-cover py-10 border-[15px] border-[#E8E0CC]"
+          {...provided.droppableProps}
+        >
+          {items.map((item, index) => (
+            <Draggable key={item.id} draggableId={item.content} index={index}>
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  style={{ cursor: snapshot.isDragging ? "grabbing" : "grab" }}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  className="bg-[#E8E0CC] bg-[url('/mapbg.png')] w-full max-w-[20rem] h-full relative aspect-square  shadow-2xl mx-auto my-auto p-[30px]"
+                >
+                  {/* <img
+                    src="/cardimages/tape.webp"
+                    className="w-[26%] max-w-[7rem] -top-[20%] left-[45%] absolute z-50"
+                  /> */}
+                  <img
+                    src="/pin.png"
+                    className="w-[26%] max-w-[7rem] absolute z-50"
+                    style={{
+                      transform: snapshot.isDragging ? "rotate(50deg)" : "rotate(0deg)",
+                      top: "8px",
+                      left: "50%",
+                      translate: "-50%",
+                      transition: "transform 0.2s ease-in-out",
+                    }}
+                  />
+                  <div className="overflow-hidden relative">
+                    <h1 className="pb-4 notes text-2xl lg:text-2xl xl:text-3xl mt-5 relative z-50 font-black text-black">
+                      {item.content}
+                    </h1>
+                  </div>
+                </div>
+              )}
+            </Draggable>
+          ))}
+
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
+  );
+};
+
+export default NotesBoard;
--- components/WhatHackathon.tsx
+++ components/WhatHackathon.tsx
@@ -1,13 +1,10 @@
-import { Definition } from "./HackathonExplain/definition";
-import { Projects } from "./HackathonExplain/projects";
-import { Vibe } from "./HackathonExplain/vibe";
-
-import { RoomProvider, useOthers, useMyPresence } from "../liveblocks.config";
-import { ReactFlashlight } from "react-flashlight";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import { useMemo } from "react";
-import Cursor from "./Cursor";
+import { RoomProvider } from "../liveblocks.config";
 import { OuijaBoard } from "./OuijaBoard";
+
+const NotesBoard = dynamic(import("./NotesBoard"));
 
 export function useOverrideRoomId(roomId: string) {
   const { query } = useRouter();
@@ -26,21 +23,11 @@ export const WhatHackathon = () => {
       <div className="flex flex-col max-w-[90rem] mx-auto divide-x-0 divide-accent-default divide-dashed">
         <div className="p-8 md:p-12">
           <h1 className="text-5xl mb-20 lg:text-7xl text-center text-accent-darker">
-            {/* What is a hackathon? */}
             Why Haunted House?
           </h1>
-          {/* <img src="/cardimages/tape.png" /> */}
-          {/* <div className="relative h-full  xl:grid-cols-3 lg:grid-cols-4 grid flex-wrap gap-10 pt-10 px-8 mx-auto max-w-8xl scale-[80%] justify-center"> */}
-          <div className="w-full px-8 lg:px-0 grid grid-cols-1 md:grid-cols-2 lg:flex flex-wrap lg:flex-nowrap gap-16  lg:gap-8 max-w-6xl mx-auto bg-[url('/corkboard.webp')] bg-cover py-10 border-[15px] border-[#E8E0CC]">
-            {/* <img
-              src="/stacked1.png"
-              className="absolute opacity-0 sm:opacity-100  scale-105 aspect-[83/100] mx-auto my-auto"
-            />
-            <img
-              src="/stacked2.png"
-              className="absolute opacity-0 sm:opacity-100 scale-105 aspect-[83/100] mx-auto my-auto"
-            /> */}
-             <div className="bg-[#E8E0CC] bg-[url('/mapbg.png')] w-full max-w-[20rem] h-full relative aspect-square  shadow-2xl mx-auto my-auto p-[30px] ml-10">
+          <NotesBoard />
+          {/* <div className="w-full px-8 lg:px-0 grid grid-cols-1 md:grid-cols-2 lg:flex flex-wrap lg:flex-nowrap gap-16  lg:gap-8 max-w-6xl mx-auto bg-[url('/corkboard.webp')] bg-cover py-10 border-[15px] border-[#E8E0CC]">
+            <div className="bg-[#E8E0CC] bg-[url('/mapbg.png')] w-full max-w-[20rem] h-full relative aspect-square  shadow-2xl mx-auto my-auto p-[30px] ml-10">
               <img
                 src="/cardimages/tape.webp"
                 className="w-[26%] max-w-[7rem] -top-[20%] left-[45%] absolute z-50"
@@ -50,18 +37,6 @@ export const WhatHackathon = () => {
                   1. Come up with an idea
                 </h1>
               </div>
-              {/* <div className="w-full h-full absolute top-0 left-0 overflow-hidden">
-                <img
-                  src="/cardimages/witch.webp"
-                  className="absolute z-0 -left-5 -bottom-5 w-[25%]"
-                />
-              </div>
-              <div className="w-full h-full absolute top-0 left-0 overflow-hidden">
-                <img
-                  src="/cardimages/web.webp"
-                  className="absolute z-0 -right-10  -bottom-10 w-[40%]"
-                />
-              </div> */}
             </div>
 
             <div className="bg-[#E8E0CC]  bg-[url('/mapbg.png')] w-full max-w-[20rem] h-full relative aspect-square  shadow-2xl mx-auto my-auto p-[30px]">
@@ -74,18 +49,6 @@ export const WhatHackathon = () => {
                   2. Form a team
                 </h1>
               </div>
-              {/* <div className="w-full h-full absolute top-0 left-0 overflow-hidden">
-                <img
-                  src="/cardimages/witch.webp"
-                  className="absolute z-0 -left-5 -bottom-5 w-[25%]"
-                />
-              </div>
-              <div className="w-full h-full absolute top-0 left-0 overflow-hidden">
-                <img
-                  src="/cardimages/web.webp"
-                  className="absolute z-0 -right-10  -bottom-10 w-[40%]"
-                />
-              </div> */}
             </div>
 
             <div className="bg-[#E8E0CC] bg-[url('/mapbg.png')] w-full max-w-[20rem] h-full relative aspect-square  shadow-2xl mx-auto my-auto p-[30px]">
@@ -98,18 +61,6 @@ export const WhatHackathon = () => {
                   3. Build and learn
                 </h1>
               </div>
-              {/* <div className="w-full h-full absolute top-0 left-0 overflow-hidden">
-                <img
-                  src="/cardimages/witch.webp"
-                  className="absolute z-0 -left-5 -bottom-5 w-[25%]"
-                />
-              </div>
-              <div className="w-full h-full absolute top-0 left-0 overflow-hidden">
-                <img
-                  src="/cardimages/web.webp"
-                  className="absolute z-0 -right-10  -bottom-10 w-[40%]"
-                />
-              </div> */}
             </div>
 
             <div className="bg-[#E8E0CC]  bg-[url('/mapbg.png')] w-full max-w-[20rem] h-full relative aspect-square  shadow-2xl mx-auto my-auto p-[30px] mr-10">
@@ -122,24 +73,11 @@ export const WhatHackathon = () => {
                   4. Show off what you made
                 </h1>
               </div>
-              {/* <div className="w-full h-full absolute top-0 left-0 overflow-hidden">
-                <img
-                  src="/cardimages/witch.webp"
-                  className="absolute z-0 -left-5 -bottom-5 w-[25%]"
-                />
-              </div>
-              <div className="w-full h-full absolute top-0 left-0 overflow-hidden">
-                <img
-                  src="/cardimages/web.webp"
-                  className="absolute z-0 -right-10  -bottom-10 w-[40%]"
-                />
-              </div> */}
             </div>
-          </div>
+          </div> */}
         </div>
         <div className="p-8 md:p-12 w-full">
           <div className="space-y-8 max-w-3xl mx-auto">
-            {/* <Definition /> */}
             <RoomProvider
               id={roomId}
               initialPresence={{
